d57a15752bd19554d566efb2d46a734fb47da9c9
JPedroD <125930800+JPedroD@users.noreply.github.com>
2023-02-25 12:29:53 -0300

    Add files via upload

---
+++ LogicaPhP/index.php
@@ -0,0 +1,11 @@
+<?php
+	$helloWorld = ['H','-','e','-','l','-','l','-','o'];
+	$string = '';
+	for($i = 0; $i < count($helloWorld); $i++){
+		if($helloWorld[$i] =='-'){
+			continue;
+		}
+		$string.=$helloWorld[$i];
+	}
+	echo $string;
+?>
